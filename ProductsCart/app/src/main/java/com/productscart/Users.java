1ded04ff7483dcf368af08db5e9de46774c6c0ed
rahulk9991 <rahulkondapally999@gmail.com>
2020-11-25 16:43:29 -0500

    commited changes

---
+++ ProductsCart/app/src/main/java/com/productscart/Users.java
@@ -0,0 +1,44 @@
+package com.productscart;
+
+public class Users {
+
+
+    public Users(String email, String phone, String password) {
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String email;
+    private String phone;
+    private String password;
+
+
+    public Users() {
+
+    }
+}
